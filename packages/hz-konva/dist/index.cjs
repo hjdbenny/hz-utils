63c465cafd8b08cc91cfbc200e477cc320e4892f
hjd <479925494@qq.com>
2025-04-20 00:46:48 +0800

    feat:脚本修改

--- packages/hz-konva/dist/index.cjs
+++ packages/hz-konva/dist/index.cjs
@@ -1,1 +1,1 @@
-"use strict";var t=require("events");module.exports=class{constructor(){this.event=new t,this.init()}init(){this.event.emit("init","HzKonva init")}on(t,e){this.event.on(t,e)}};
+"use strict";var t=require("events");module.exports=class{constructor(){this.event=new t,this.init()}init(){setTimeout((()=>{this.event.emit("init","HzKonva init")}))}on(t,e){this.event.on(t,(t=>{e(t)}))}};
--- packages/hz-konva/dist/index.esm.js
+++ packages/hz-konva/dist/index.esm.js
@@ -1,1 +1,1 @@
-import t from"events";class i{constructor(){this.event=new t,this.init()}init(){this.event.emit("init","HzKonva init")}on(t,i){this.event.on(t,i)}}export{i as default};
+import t from"events";class i{constructor(){this.event=new t,this.init()}init(){setTimeout((()=>{this.event.emit("init","HzKonva init")}))}on(t,i){this.event.on(t,(t=>{i(t)}))}}export{i as default};
